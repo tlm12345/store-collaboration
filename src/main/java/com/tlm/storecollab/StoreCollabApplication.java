37a481d64468e2e74900240b510d32621115775e
tlm <1248369077@qq.com>
2025-05-13 22:57:43 +0800

    Project initialization

--- src/main/java/com/tlm/storecollab/StoreCollabApplication.java
+++ src/main/java/com/tlm/storecollab/StoreCollabApplication.java
@@ -4,6 +4,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * store-collaboration Project
+ */
 @SpringBootApplication
 @MapperScan("com.tlm.storecollab.mapper")
 public class StoreCollabApplication {
